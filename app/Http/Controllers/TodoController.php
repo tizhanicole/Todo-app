be29667fcc21ca0b2b1e5afe6636c3cd708384b0
tizhanicole <penamantetizha@gmail.com>
2023-11-01 12:37:22 +0800

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return \Illuminate\Htttp\Response
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Todo::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param int $id
+     * @return \Illuminate\Htpp\Response
+     */
+    public function show($id)
+    {
+        $todo = Todo::find($id);
+        return $todo;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, string $id)
+    {
+        $todo = Todo::find($id);
+        $todo->update($request->all());
+        return $todo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(string $id)
+    {
+        return Todo::destroy($id);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// get todos list
+Route::get('/todos', [TodoController::class, 'index']);
+
+//retrieve a single todo
+Route::get('/todos/{id}', [TodoController::class, 'show']);
+
+//create a todo
+Route::post('/todos', [TodoController::class, 'store']);
+
+//update a todo
+Route::put('/todos/{id}', [TodoController::class, 'update']);
+
+//delete a todo
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
